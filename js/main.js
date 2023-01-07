f95159ac4484b57bb040cde55eeb414603a8b254
Zama <zama.barsaev@gmail.com>
2023-01-07 21:57:54 +0300

    safsaf

--- js/main.js
+++ js/main.js
@@ -1,4 +1,72 @@
 document.addEventListener("DOMContentLoaded", () => {
+  /* Burger - Nav */
+  const burgerActive = () => {
+    const burger = document.querySelector(".header__burger"),
+      nav = document.querySelector(".nav"),
+      links = document.querySelectorAll(".nav__link"),
+      body = document.getElementsByTagName("body");
+
+    burger.addEventListener("click", (e) => {
+      e.currentTarget.classList.toggle("active");
+      nav.classList.toggle("active");
+      body[0].classList.toggle("active");
+    });
+
+    for (let i = 0; links.length > i; ++i) {
+      links[i].addEventListener("click", (e) => {
+        for (i = 0; links.length > i; ++i) {
+          links[i].classList.remove("active");
+        }
+        e.currentTarget.classList.add("active");
+        nav.classList.remove("active");
+        burger.classList.remove("active");
+        body[0].classList.remove("active");
+      });
+    }
+  };
+  burgerActive();
+
+  /* Active Link Of Scroll */
+  const links = document.querySelectorAll(".nav__link"),
+    section = document.querySelectorAll(".section"),
+    header = document.querySelector(".header__head");
+  const navInit = (link, section, header) => {
+    section.forEach((section) => {
+      if (window.pageYOffset + header.offsetHeight + 1 >= section.offsetTop) {
+        link.forEach((li) => {
+          li.classList.remove("active");
+          if (li.dataset.section === section.dataset.section) {
+            li.classList.add("active");
+          }
+        });
+      }
+    });
+  };
+  navInit(links, section, header);
+  window.addEventListener("scroll", () => {
+    navInit(links, section, header);
+  });
+  window.addEventListener("resize", () => {
+    navInit(links, section, header);
+  });
+
+  /* Burger On Scroll */
+  window.onscroll = () => {
+    const header = document.querySelector(".header__head"),
+      up = document.querySelector(".up");
+    if (window.scrollY > 10) {
+      header.classList.add("scroll");
+    } else if (window.scrollY < 10) {
+      header.classList.remove("scroll");
+    }
+
+    if (window.scrollY > 200) {
+      up.classList.add("active");
+    } else if (window.scrollY < 200) {
+      up.classList.remove("active");
+    }
+  };
+
   /* Accordeon */
   const aboutUsHead = document.querySelectorAll(".about-us__accordeon-head"),
     aboutUsBody = document.querySelectorAll(".about-us__accordeon-body"),
@@ -72,5 +140,24 @@ document.addEventListener("DOMContentLoaded", () => {
       dynamicBullets: true,
       dynamicMainBullets: 3,
     },
+  });
+});
+
+/* Scroll On Link */
+$(function () {
+  $("[data-scroll").click(function (event) {
+    event.preventDefault();
+
+    let blockId = $(this).data("scroll"),
+      header = document.querySelector(".header__head");
+
+    let blockOffset = $(blockId).offset().top - header.offsetHeight;
+
+    $("html, body").animate(
+      {
+        scrollTop: blockOffset,
+      },
+      900
+    );
   });
 });
